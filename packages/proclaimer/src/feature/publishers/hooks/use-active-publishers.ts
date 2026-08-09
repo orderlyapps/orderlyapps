b1a1b00866f7039852ea0fe9feed874762814d7b
orderlyapps <orderlyapps@gmail.com>
2026-08-09 19:55:27 +1000

    refactor: remove unused publisher filter hooks and tests
    - Delete useActivePublishers, useArchivedPublishers, usePublishersByFamily, usePublishersByGender, usePublishersByGroup, usePublishersByStanding, and usePublishersByType hooks
    - Remove corresponding test file use-filter-hooks.test.tsx
    - These hooks were not being used and can be recreated with useFilteredPublishers when needed

--- packages/proclaimer/src/feature/publishers/hooks/use-active-publishers.ts
+++
@@ -1,7 +0,0 @@
-import { isNull } from "@tanstack/react-db";
-import type { UsePublishersResult } from "./use-publishers-base.js";
-import { useFilteredPublishers } from "./use-filtered-publishers.js";
-
-export function useActivePublishers(): UsePublishersResult {
-  return useFilteredPublishers((publisher) => isNull(publisher.archived_at), []);
-}
