e38162c702f3470b906b21277d1f61335ace195e
Damian Amodeo <orderlyapps@gmail.com>
2026-09-24 18:17:46 +1000

    refactor: inline ServiceOverseerHeader and ServiceOverseerContent components into ServiceOverseerPage
    - Delete ServiceOverseerHeader and ServiceOverseerContent components from service-overseer directory
    - Inline ServiceOverseerHeader toolbar directly into ServiceOverseerPage component
    - Inline ServiceOverseerContent navigation list directly into ServiceOverseerPage component
    - Add IonToolbar, IonTitle, IonButtons, IonBackButton, and IonList imports to ServiceOverseerPage

--- apps/proclaimer/src/routes/home/service-overseer/ServiceOverseer.tsx
+++ apps/proclaimer/src/routes/home/service-overseer/ServiceOverseer.tsx
@@ -1,15 +1,34 @@
-import { IonPage, IonHeader, IonContent } from "@ionic/react";
-import { ServiceOverseerHeader } from "@proclaimer-content/pages/home/service-overseer/service-overseer-header/ServiceOverseerHeader";
-import { ServiceOverseerContent } from "@proclaimer-content/pages/home/service-overseer/service-overseer-content/ServiceOverseerContent";
+import {
+  IonPage,
+  IonHeader,
+  IonContent,
+  IonToolbar,
+  IonTitle,
+  IonButtons,
+  IonBackButton,
+  IonList,
+} from "@ionic/react";
+import { NavItem } from "@amodeo/proclaimer/ui/components/navigation/nav-item/NavItem";
 
 function ServiceOverseerPage() {
   return (
     <IonPage>
       <IonHeader>
-        <ServiceOverseerHeader />
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonBackButton />
+          </IonButtons>
+          <IonTitle>Service Overseer</IonTitle>
+        </IonToolbar>
       </IonHeader>
       <IonContent>
-        <ServiceOverseerContent />
+        <IonList>
+          <NavItem label="Map" to="/home/service-overseer/map" />
+          <NavItem label="Records" to="/home/service-overseer/map-log" />
+          <NavItem label="Tags" to="/home/service-overseer/map-tags" />
+          <NavItem label="PDFs" to="/home/service-overseer/pdfs" />
+          {/* <NavItem label="Auto Checkout" to="/home/service-overseer/map-checkout" /> */}
+        </IonList>
       </IonContent>
     </IonPage>
   );
